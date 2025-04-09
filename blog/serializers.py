c857aeaf82d17c8d5c86924f893ef44c3a922a33
Somer-H <233424@ids.upchiapas.edu.mx>
2025-04-08 18:18:12 -0600

    animals

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,15 +4,30 @@ from .models import Animal, Level, UserProgress, CustomAnimalImage
 class AnimalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Animal
-        fields = ['id', 'name', 'starting_vowel', 'image']
+        fields = ['name', 'starting_vowel', 'image']  # Excluye 'id'
 
 class LevelSerializer(serializers.ModelSerializer):
-    animals = AnimalSerializer(many=True, read_only=True)
-    
+    # Usamos PrimaryKeyRelatedField para POST (IDs) y AnimalSerializer para GET (detalles)
+    animals = AnimalSerializer(many=True, read_only=True)  # Solo lectura (detalles en la respuesta)
+    animal_ids = serializers.PrimaryKeyRelatedField(queryset=Animal.objects.all(), many=True, write_only=True)  # Solo escritura (IDs en la solicitud)
+
     class Meta:
         model = Level
-        fields = ['id', 'name', 'description', 'difficulty', 'animals']
-
+        fields = ['id', 'name', 'description', 'difficulty', 'animals', 'animal_ids']
+    
+    def create(self, validated_data):
+        animal_ids = validated_data.pop('animal_ids', [])
+        level = Level.objects.create(**validated_data)
+        level.animals.set(animal_ids)  # Relacionamos los IDs con el nivel
+        return level
+    
+    def update(self, instance, validated_data):
+        animal_ids = validated_data.pop('animal_ids', [])
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        instance.animals.set(animal_ids)  # Actualizamos las relaciones con los animales
+        return instance
 class UserProgressSerializer(serializers.ModelSerializer):
     level_name = serializers.CharField(source='level.name', read_only=True)
     
--- myproyecto/settings.py
+++ myproyecto/settings.py
@@ -78,9 +78,9 @@ WSGI_APPLICATION = 'myproyecto.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'webhookuse',
+        'NAME': 'webhook',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '140823schmesom1917',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
